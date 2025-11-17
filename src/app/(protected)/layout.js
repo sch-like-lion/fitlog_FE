ced66cac45b5e979a964867cab63e854639c8d3e
jeongchaehwan <wjdcoghks1428@naver.com>
2025-11-17 17:12:00 +0900

    회원가입 api 수정중, 윤오님이 올려주신 브랜치랑 병합 시도

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -1,16 +1,16 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+  "use client";
+  import { useEffect } from "react";
+  import { useRouter } from "next/navigation";
 
-export default function ProtectedLayout({ children }) {
-  const router = useRouter();
-  useEffect(() => {
-    console.log('router바뀜!!!');
-    const token = localStorage.getItem("accessToken");
-    // if (!token) {
-    //   router.replace("/login");
-    // }
-  }, [router]);
+  export default function ProtectedLayout({ children }) {
+    const router = useRouter();
+    useEffect(() => {
+      console.log('router바뀜!!!');
+      const token = localStorage.getItem("accessToken");
+      // if (!token) {
+      //   router.replace("/login");
+      // }
+    }, [router]);
 
-  return <>{children}</>;
-}
+    return <>{children}</>;
+  }
