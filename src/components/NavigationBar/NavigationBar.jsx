078d706ccd339f0092c119138f8531f3bd89b008
takyujin <tyujin@naver.com>
2025-11-18 01:30:35 +0900

    네비게이션

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
-import "./NavigationBar.css";
+// import "./NavigationBar.css";
 import { usePathname } from "next/navigation";
 
 export default function NavigationBar() {
